b2fb95a809ba4b357dfd1e53ea3b2e067888035c
Stefan Ilic <silic@stu.naperville203.org>
2024-01-24 19:10:01 -0600

    start intake, create ios

---
+++ src/main/java/frc/robot/subsystems/intake/IntakeConstants.java
@@ -0,0 +1,11 @@
+package frc.robot.subsystems.intake;
+
+public class IntakeConstants {
+    public static final int INTAKE_RIGHT_ROLLER_MOTOR_ID = 0;
+    public static final int INTAKE_LEFT_ROLLER_MOTOR_ID = 1;
+    public static final int INTAKE_DRUM_MOTOR_ID = 2;
+
+    public static final int INTAKE_RIGHT_ROLLER_IR_SENSOR_ID = 3;
+    public static final int INTAKE_LEFT_ROLLER_IR_SENSOR_ID = 4;
+    public static final int INTAKE_DRUM_IR_SENSOR_ID = 5;    
+}
