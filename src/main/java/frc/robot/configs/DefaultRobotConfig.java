4eb246b026100e3ac1464b44d441178a7cfd3bc9
tyler51 <77416087+tyler51@users.noreply.github.com>
2024-02-15 19:47:27 -0600

    Updated and added constants for shooter

--- src/main/java/frc/robot/configs/DefaultRobotConfig.java
+++ src/main/java/frc/robot/configs/DefaultRobotConfig.java
@@ -13,27 +13,33 @@ import frc.lib.team3061.drivetrain.swerve.SwerveConstants;
 public class DefaultRobotConfig extends RobotConfig {
 
   // FIXME: update all CAN IDs and steer offsets
-  private static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 13;
-  private static final int FRONT_LEFT_MODULE_STEER_MOTOR = 12;
-  private static final int FRONT_LEFT_MODULE_STEER_ENCODER = 14;
+  private static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
+  private static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2;
+  private static final int FRONT_LEFT_MODULE_STEER_ENCODER = 22;
   private static final double FRONT_LEFT_MODULE_STEER_OFFSET_ROT = -0.22591;
 
-  private static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 16;
-  private static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 15;
-  private static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 17;
+  private static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
+  private static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
+  private static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 23;
   private static final double FRONT_RIGHT_MODULE_STEER_OFFSET_ROT = -0.390381;
 
   private static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 7;
-  private static final int BACK_LEFT_MODULE_STEER_MOTOR = 6;
-  private static final int BACK_LEFT_MODULE_STEER_ENCODER = 8;
+  private static final int BACK_LEFT_MODULE_STEER_MOTOR = 8;
+  private static final int BACK_LEFT_MODULE_STEER_ENCODER = 24;
   private static final double BACK_LEFT_MODULE_STEER_OFFSET_ROT = 0.327393;
 
-  private static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 10;
-  private static final int BACK_RIGHT_MODULE_STEER_MOTOR = 9;
-  private static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11;
+  private static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 5;
+  private static final int BACK_RIGHT_MODULE_STEER_MOTOR = 6;
+  private static final int BACK_RIGHT_MODULE_STEER_ENCODER = 25;
   private static final double BACK_RIGHT_MODULE_STEER_OFFSET_ROT = -0.336670;
 
-  private static final int GYRO_ID = 3;
+  // Shooter IDs
+  private static final int SHOOTER_KICKER = 21;
+  private static final int SHOOTER_TOP_MOTOR = 27;
+  private static final int SHOOTER_BOTTOM_MOTOR = 28;
+  private static final int SHOOTER_PIVOT_MOTOR = 29;
+
+  private static final int GYRO_ID = 26; // FIXME: Update gyro ID
 
   // FIXME: update robot dimensions
   private static final double TRACKWIDTH_METERS = 0.523875; // 20.625
