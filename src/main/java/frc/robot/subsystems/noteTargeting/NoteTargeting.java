f8e728cdb3e6988be68a6bd9e63737d93aa9bce1
Luke Golobitsh <lwgolobitsh@stu.naperville203.org>
2024-02-19 11:25:13 -0600

    Make Tunable for kP, kI, kD

--- src/main/java/frc/robot/subsystems/noteTargeting/NoteTargeting.java
+++ src/main/java/frc/robot/subsystems/noteTargeting/NoteTargeting.java
@@ -2,14 +2,20 @@ package frc.robot.subsystems.noteTargeting;
 
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.lib.team6328.util.TunableNumber;
+
 import org.littletonrobotics.junction.Logger;
 
 public class NoteTargeting extends SubsystemBase {
+
+  private final TunableNumber kP = new TunableNumber("NoteTargeting/kP", 0.1);
+  private final TunableNumber kI = new TunableNumber("NoteTargeting/kI", 0.0);
+  private final TunableNumber kD = new TunableNumber("NoteTargeting/kD", 0.0);
   private final NoteTargetingIO leftio;
   private final NoteTargetingIO rightio;
   public final String SUBSYSTEM_NAME = "NoteTargeting";
   private NoteTargetingIOInputsAutoLogged primaryTarget = null;
-  private PIDController pid = new PIDController(0.1, 0.0, 0.1);
+  private PIDController pid = new PIDController(kP.get(), kI.get(), kD.get());
   private NoteTargetingIOInputsAutoLogged leftInputs = new NoteTargetingIOInputsAutoLogged();
   private NoteTargetingIOInputsAutoLogged rightInputs = new NoteTargetingIOInputsAutoLogged();
 
@@ -39,6 +45,9 @@ public class NoteTargeting extends SubsystemBase {
     rightio.updateInputs(rightInputs);
     assignPrimaryTarget();
     handleLEDs();
+    pid.setP(kP.get());
+    pid.setI(kI.get());
+    pid.setD(kD.get());
     Logger.processInputs(SUBSYSTEM_NAME + "/left", leftInputs);
     Logger.processInputs(SUBSYSTEM_NAME + "/right", rightInputs);
   }
