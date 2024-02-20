4fbccd3781193e1295fafba8f8a8eca43d7d993a
Geoff Schmit <gcschmit@gmail.com>
2024-02-20 11:43:42 -0600

    tune ambiguity and max pose difference

--- src/main/java/frc/lib/team3061/vision/VisionConstants.java
+++ src/main/java/frc/lib/team3061/vision/VisionConstants.java
@@ -17,12 +17,12 @@ public final class VisionConstants {
   public static final String SUBSYSTEM_NAME = "Vision";
 
   // the pose ambiguity must be less than this value for the target to be considered valid
-  public static final double MAXIMUM_AMBIGUITY = 0.2;
+  public static final double MAXIMUM_AMBIGUITY = 0.15;
 
   // the maximum distance between the robot's pose derived from the target and the current robot's
   // estimated pose for the target to be used to update the robot's pose (essentially, always use a
   // valid target to update the robot's pose)
-  public static final double MAX_POSE_DIFFERENCE_METERS = 1000.0;
+  public static final double MAX_POSE_DIFFERENCE_METERS = 2.0;
 
   // the maximum distance between the robot and the target, for the target to be used to update the
   // robot's pose
