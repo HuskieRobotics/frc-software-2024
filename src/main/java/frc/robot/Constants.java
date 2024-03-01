0055fb7d734750f313a00c2864e9d38d3094fe01
Aayush Modi <ambase6@gmail.com>
2024-03-01 01:22:41 -0600

    add new robot type

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -37,7 +37,8 @@ public final class Constants {
     ROBOT_SIMBOT,
     ROBOT_SIMBOT_CTRE,
     ROBOT_PRACTICE,
-    ROBOT_COMPETITION
+    ROBOT_COMPETITION,
+    ROBOT_PRACTICE_BOARD
   }
 
   public static RobotType getRobot() {
@@ -57,6 +58,7 @@ public final class Constants {
   public static Mode getMode() {
     switch (getRobot()) {
       case ROBOT_PRACTICE:
+      case ROBOT_PRACTICE_BOARD:
       case ROBOT_COMPETITION:
         return RobotBase.isReal() ? Mode.REAL : Mode.REPLAY;
 
