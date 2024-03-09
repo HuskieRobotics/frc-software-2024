42452064ddc8cb4a5557fafe0a5a608099161d4c
Stefan Ilic <111461725+StefanIlic1@users.noreply.github.com>
2024-03-09 14:49:18 -0600

    Updated led patterns (#33)
    * update led patterns for new software feature sheet
    * change to fast wave
    * update for gbr leds
    * fix leds with GRB and make fire animations
    * update for pink wave
    * make new fire and rainbow animations
    * make orange pulse
    * delete broken "rainbow wave"
    
    ---------

--- src/main/java/frc/lib/team3061/leds/LEDsRIO.java
+++ src/main/java/frc/lib/team3061/leds/LEDsRIO.java
@@ -8,10 +8,14 @@ public class LEDsRIO extends LEDs {
 
   private final AddressableLED leds;
   private final AddressableLEDBuffer buffer;
+  private boolean isGRB;
 
   protected LEDsRIO() {
     leds = new AddressableLED(0);
     buffer = new AddressableLEDBuffer(ACTUAL_LENGTH);
+    // leds.setBitTiming(500, 200, 1200, 1300);
+    isGRB = true;
+
     leds.setLength(ACTUAL_LENGTH);
     leds.setData(buffer);
     leds.start();
@@ -24,9 +28,17 @@ public class LEDsRIO extends LEDs {
 
   @Override
   protected void setLEDBuffer(int index, Color color) {
+    if (isGRB) {
+      color = changeToGRB(color);
+    }
+
     buffer.setLED(index, color);
     if (MIRROR_LEDS) {
       buffer.setLED(ACTUAL_LENGTH - index - 1, color);
     }
+  }
+
+  static Color changeToGRB(Color color) {
+    return new Color(color.green, color.red, color.blue);
   }
 }
