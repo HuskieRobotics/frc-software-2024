5e1a1140fa5c720adc236a1c0b66dae61a7e33ed
Luke Golobitsh <73967796+lwgolobitsh@users.noreply.github.com>
2024-03-29 16:32:43 -0500

    Fix deflector bug

--- src/main/java/frc/robot/subsystems/shooter/Shooter.java
+++ src/main/java/frc/robot/subsystems/shooter/Shooter.java
@@ -185,6 +185,8 @@ public class Shooter extends SubsystemBase {
         this.setRangeVelocity(distanceToSpeaker);
         if (shootingPosition == ShootingPosition.AMP) {
           deployDeflector();
+        } else {
+          retractDeflector();
         }
       }
     }
