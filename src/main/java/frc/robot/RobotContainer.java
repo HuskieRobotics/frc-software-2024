0c8a23f62e2f2bf7040aa498f38fd54f8adf7043
Luke Golobitsh <73967796+lwgolobitsh@users.noreply.github.com>
2024-03-30 16:07:19 -0500

    add 4 note source side red + formatting

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -392,7 +392,7 @@ public class RobotContainer {
      *
      */
 
-    Command fourNoteSourceSide =
+    Command fourNoteSourceSideBlue =
         Commands.sequence(
             Commands.runOnce(() -> shooter.setShootingPosition(ShootingPosition.SOURCE_SIDE_AUTO)),
             new PathPlannerAuto("Collect 2nd"),
@@ -409,7 +409,26 @@ public class RobotContainer {
             Commands.runOnce(
                 () -> shooter.setShootingPosition(ShootingPosition.SOURCE_SIDE_UNDER_STAGE_AUTO)),
             new PathPlannerAuto("Score 4th Center"));
-    autoChooser.addOption("4 Note Source Side", fourNoteSourceSide);
+    autoChooser.addOption("4 Note Source Side Blue", fourNoteSourceSideBlue);
+
+    Command fourNoteSourceSideRed =
+        Commands.sequence(
+            Commands.runOnce(() -> shooter.setShootingPosition(ShootingPosition.SOURCE_SIDE_AUTO)),
+            new PathPlannerAuto("Collect 2nd Red"),
+            Commands.either(
+                new PathPlannerAuto("Score 2nd Collect 3rd Red"),
+                new PathPlannerAuto("Missed 2nd Collect 3rd Red"),
+                intake::hasNoteForAuto),
+            Commands.runOnce(
+                () -> shooter.setShootingPosition(ShootingPosition.SOURCE_SIDE_UNDER_STAGE_AUTO)),
+            Commands.either(
+                new PathPlannerAuto("Score 3rd Collect 4th Red"),
+                new PathPlannerAuto("Missed 3rd Collect 4th Red"),
+                intake::hasNoteForAuto),
+            Commands.runOnce(
+                () -> shooter.setShootingPosition(ShootingPosition.SOURCE_SIDE_UNDER_STAGE_AUTO)),
+            new PathPlannerAuto("Score 4th Center"));
+    autoChooser.addOption("4 Note Source Side Red", fourNoteSourceSideRed);
 
     /************ 6 Note Amp Side ************
      *
@@ -751,8 +770,7 @@ public class RobotContainer {
                 .until(
                     () -> {
                       return !intake.hasNote();
-                    }))
-                    ;
+                    }));
 
     // oi.getPrepareToScoreAmpButton()
     //     .onTrue(
