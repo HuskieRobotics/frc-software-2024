919a66a949fddf301644400ddb46bd27e72c2394
Stefan Ilic <111461725+StefanIlic1@users.noreply.github.com>
2024-04-01 19:10:47 -0500

    update pose estimation for slipping

--- src/main/java/frc/lib/team3061/drivetrain/Drivetrain.java
+++ src/main/java/frc/lib/team3061/drivetrain/Drivetrain.java
@@ -30,10 +30,14 @@ import frc.lib.team3015.subsystem.FaultReporter;
 import frc.lib.team3061.RobotConfig;
 import frc.lib.team3061.drivetrain.DrivetrainIO.SwerveIOInputs;
 import frc.lib.team6328.util.Alert;
+import frc.lib.team6328.util.FieldConstants;
 import frc.lib.team6328.util.Alert.AlertType;
 import frc.lib.team6328.util.TunableNumber;
 import frc.robot.Constants;
 import frc.robot.Field2d;
+
+import java.lang.reflect.Field;
+import java.util.Optional;
 import java.util.function.Supplier;
 import org.littletonrobotics.junction.Logger;
 
@@ -476,6 +480,34 @@ public class Drivetrain extends SubsystemBase {
       this.prevRobotPose = this.inputs.drivetrain.robotPose;
       Logger.recordOutput(SUBSYSTEM_NAME + "/Teleported", false);
     }
+
+    // check for position outside the field due to slipping
+    if (this.inputs.drivetrain.robotPose.getX() < 0) {
+      this.resetPose(new Pose2d(
+          0,
+          this.inputs.drivetrain.robotPose.getY(),
+          this.inputs.drivetrain.robotPose.getRotation()
+      ));
+    } else if (this.inputs.drivetrain.robotPose.getX() > FieldConstants.fieldLength) {
+      this.resetPose(new Pose2d(
+          FieldConstants.fieldLength,
+          this.inputs.drivetrain.robotPose.getY(),
+          this.inputs.drivetrain.robotPose.getRotation()
+      ));
+    } else if (this.inputs.drivetrain.robotPose.getY() < 0) {
+      this.resetPose(new Pose2d(
+          this.inputs.drivetrain.robotPose.getX(),
+          0,
+          this.inputs.drivetrain.robotPose.getRotation()
+      ));
+    } else if (this.inputs.drivetrain.robotPose.getY() > FieldConstants.fieldWidth) {
+      this.resetPose(new Pose2d(
+          this.inputs.drivetrain.robotPose.getX(),
+          FieldConstants.fieldWidth,
+          this.inputs.drivetrain.robotPose.getRotation()
+      ));
+    }
+
 
     // update the brake mode based on the robot's velocity and state (enabled/disabled)
     updateBrakeMode();
