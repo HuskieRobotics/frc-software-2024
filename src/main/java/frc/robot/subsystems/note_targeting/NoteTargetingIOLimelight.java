b2402a911d2b088edd4c31e24a4af9d0702736ab
Connor Barbaccia <73967796+lwgolobitsh@users.noreply.github.com>
2024-04-03 20:01:37 -0500

    Revert "implement target queue to handle loss of sight"
    This reverts commit b3f332d5645fa67a9065a803409969ddff58fb5d.

--- src/main/java/frc/robot/subsystems/note_targeting/NoteTargetingIOLimelight.java
+++ src/main/java/frc/robot/subsystems/note_targeting/NoteTargetingIOLimelight.java
@@ -3,10 +3,6 @@ package frc.robot.subsystems.note_targeting;
 import edu.wpi.first.networktables.NetworkTable;
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import frc.lib.team6328.util.TunableNumber;
-
-import java.util.LinkedList;
-import java.util.Queue;
 
 public class NoteTargetingIOLimelight implements NoteTargetingIO {
   private String name;
@@ -15,13 +11,8 @@ public class NoteTargetingIOLimelight implements NoteTargetingIO {
   private NetworkTableEntry ty;
   private NetworkTableEntry ta;
   private int noTargetCount;
-  private final TunableNumber targetAreaThreshold = new TunableNumber("NoteTargeting/targetAreaThreshold", 0.15);
-  private final TunableNumber samplingPeriod = new TunableNumber("NoteTargeting/samplingPeriod", 0.5);
-  private final TunableNumber targetPercentage = new TunableNumber("NoteTargeting/targetPercentage", 0.5);
-  private Queue<Boolean> targetQueue;
 
   public NoteTargetingIOLimelight(String paramName) {
-    
     if (paramName.equals("test")) {
       this.name = "test";
     } else {
@@ -30,10 +21,8 @@ public class NoteTargetingIOLimelight implements NoteTargetingIO {
       tx = table.getEntry("tx");
       ty = table.getEntry("ty");
       ta = table.getEntry("ta");
-      targetQueue = new LinkedList<>(null);
-
-      };
     }
+  }
 
   @Override
   public void updateInputs(NoteTargetingIOInputs inputs) {
@@ -43,39 +32,18 @@ public class NoteTargetingIOLimelight implements NoteTargetingIO {
       inputs.a = 0.0;
       inputs.hasTarget = false;
     } else {
-
       inputs.x = -tx.getDouble(0.0);
       inputs.y = ty.getDouble(0.0);
       inputs.a = ta.getDouble(0.0);
-      boolean seesTarget = ta.getDouble(0.0) > targetAreaThreshold.get();
-      if (seesTarget) {
-        //if the target is seen and the queue is full, remove the oldest value
-        if (targetQueue.size() >= (int) (samplingPeriod.get()*50)) {
-          targetQueue.poll();
-        }
-        //add the new value to the queue
-        targetQueue.add(true);
+      boolean hasTarget = ta.getDouble(0.0) > 0;
+      if (hasTarget) {
+        inputs.hasTarget = true;
+        this.noTargetCount = 0;
       } else {
-        //if the target is not seen and the queue is full, remove the oldest value
-        if (targetQueue.size() >= (int) (samplingPeriod.get()*50)) {
-          targetQueue.poll();
+        this.noTargetCount++;
+        if (this.noTargetCount > 10) {
+          inputs.hasTarget = false;
         }
-        //add the new value to the queue
-        targetQueue.add(false);
-      }
-      //if the percentage of the queue that is true is greater than the target percentage, set hasTarget to true
-      //iterate the queue and count the number of true values 
-      int trueCount = 0;
-      for (Boolean b : targetQueue) {
-        if (b) {
-          trueCount++;
-        }
-      }
-      //if the percentage of the queue that is true is greater than the target percentage, set hasTarget to true
-      inputs.hasTarget = ((double) trueCount / targetQueue.size()) > targetPercentage.get();
-      //clear the queue if the target is not had
-      if (!inputs.hasTarget) {
-        targetQueue.clear();
       }
     }
   }
