c1fbe811368dd0c62cb43441c62ef610e0be0767
Stefan Ilic <silic@stu.naperville203.org>
2024-04-13 09:15:38 -0500

    reset climber encoder on each redeploy

--- src/main/java/frc/robot/subsystems/climber/ClimberIOTalonFX.java
+++ src/main/java/frc/robot/subsystems/climber/ClimberIOTalonFX.java
@@ -56,6 +56,8 @@ public class ClimberIOTalonFX implements ClimberIO {
     climberTemperatureStatusSignal = climberMotor.getDeviceTemp();
 
     configureClimberMotor(climberMotor);
+
+    climberMotor.setPosition(0.0);
   }
 
   @Override
