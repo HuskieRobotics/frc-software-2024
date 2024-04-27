28eaddcd9c5fd9e784e76ecad25be81374d65ebd
Connor Barbaccia <73967796+lwgolobitsh@users.noreply.github.com>
2024-04-27 12:50:57 -0500

    add demo mode constant

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -27,6 +27,7 @@ public final class Constants {
 
   // set to true in order to change all Tunable values via Shuffleboard
   public static final boolean TUNING_MODE = false;
+  public static final boolean DEMO_MODE = true;
 
   private static final RobotType ROBOT = RobotType.ROBOT_COMPETITION;
 
