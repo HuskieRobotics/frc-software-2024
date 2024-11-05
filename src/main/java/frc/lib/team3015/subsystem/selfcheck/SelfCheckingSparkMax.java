3fd0ce27497a14b8447840909c523dbf205f7508
Geoff Schmit <gcschmit@gmail.com>
2024-11-05 09:05:57 -0600

    Update class names and paths for RevLib 2025
    CANSparkMax is now SparkMax and in a different package (com.rev.robotics.spark.SparkMax)

--- src/main/java/frc/lib/team3015/subsystem/selfcheck/SelfCheckingSparkMax.java
+++ src/main/java/frc/lib/team3015/subsystem/selfcheck/SelfCheckingSparkMax.java
@@ -1,6 +1,6 @@
 package frc.lib.team3015.subsystem.selfcheck;
 
-import com.revrobotics.CANSparkMax;
+import com.revrobotics.spark.SparkMax;
 import com.revrobotics.REVLibError;
 import frc.lib.team3015.subsystem.SubsystemFault;
 import java.util.ArrayList;
@@ -8,9 +8,9 @@ import java.util.List;
 
 public class SelfCheckingSparkMax implements SelfChecking {
   private final String label;
-  private CANSparkMax spark;
+  private SparkMax spark;
 
-  public SelfCheckingSparkMax(String label, CANSparkMax spark) {
+  public SelfCheckingSparkMax(String label, SparkMax spark) {
     this.label = label;
     this.spark = spark;
   }
