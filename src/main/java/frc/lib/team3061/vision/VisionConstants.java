9711c842b3ecd1b27f674a95f148c6890405fcd6
Stefan Ilic <111461725+StefanIlic1@users.noreply.github.com>
2024-11-22 15:22:08 -0600

    Use new AprilTag field layout.

--- src/main/java/frc/lib/team3061/vision/VisionConstants.java
+++ src/main/java/frc/lib/team3061/vision/VisionConstants.java
@@ -12,7 +12,7 @@ public final class VisionConstants {
   }
 
   public static final Path APRILTAG_FIELD_LAYOUT_PATH =
-      new File(Filesystem.getDeployDirectory(), "2024-crescendo.json").toPath();
+      new File(Filesystem.getDeployDirectory(), "2024-preseason-vision.json").toPath();
 
   public static final String SUBSYSTEM_NAME = "Vision";
 
